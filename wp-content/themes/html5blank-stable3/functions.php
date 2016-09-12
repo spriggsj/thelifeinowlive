28dba8a070dfac743bf11b5cc019b5995430ff4b
jason spriggs <jasonspriggs73@gmail.com>
2016-09-12 09:56:50 -0700

    added navwalker and header

--- wp-content/themes/html5blank-stable3/functions.php
+++ wp-content/themes/html5blank-stable3/functions.php
@@ -63,29 +63,58 @@ if (function_exists('add_theme_support'))
 \*------------------------------------*/
 
 // HTML5 Blank navigation
+// Register Custom Navigation Walker
+ require_once('wp_bootstrap_navwalker.php');
+
+
+// HTML5 Blank navigation
 function html5blank_nav()
 {
-	wp_nav_menu(
-	array(
-		'theme_location'  => 'header-menu',
-		'menu'            => '',
-		'container'       => 'div',
-		'container_class' => 'menu-{menu slug}-container',
-		'container_id'    => '',
-		'menu_class'      => 'menu',
-		'menu_id'         => '',
-		'echo'            => true,
-		'fallback_cb'     => 'wp_page_menu',
-		'before'          => '',
-		'after'           => '',
-		'link_before'     => '',
-		'link_after'      => '',
-		'items_wrap'      => '<ul>%3$s</ul>',
-		'depth'           => 0,
-		'walker'          => ''
-		)
-	);
+    wp_nav_menu(
+    array(
+        'theme_location'  => 'header-menu',
+        'menu'            => '',
+        'container'       => 'div',
+        'container_class' => 'menu-{menu slug}-container',
+        'container_id'    => '',
+        'menu_class'      => 'menu',
+        'menu_id'         => '',
+        'echo'            => true,
+        'fallback_cb'     => 'wp_page_menu',
+        'before'          => '',
+        'after'           => '',
+        'link_before'     => '',
+        'link_after'      => '',
+        'items_wrap'      => '<ul>%3$s</ul>',
+        'depth'           => 0,
+        'walker'          => ''
+        )
+    );
 }
+
+// function html5blank_nav()
+// {
+// 	wp_nav_menu(
+// 	array(
+// 		'theme_location'  => 'header-menu',
+// 		'menu'            => '',
+// 		'container'       => 'div',
+// 		'container_class' => 'menu-{menu slug}-container',
+// 		'container_id'    => '',
+// 		'menu_class'      => 'menu',
+// 		'menu_id'         => '',
+// 		'echo'            => true,
+// 		'fallback_cb'     => 'wp_page_menu',
+// 		'before'          => '',
+// 		'after'           => '',
+// 		'link_before'     => '',
+// 		'link_after'      => '',
+// 		'items_wrap'      => '<ul>%3$s</ul>',
+// 		'depth'           => 0,
+// 		'walker'          => ''
+// 		)
+// 	);
+// }
 
 // Load HTML5 Blank scripts (header.php)
 function html5blank_header_scripts()
