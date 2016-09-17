984f431b9e712917d86c8f942b213b0228e51da2
jason spriggs <jasonspriggs73@gmail.com>
2016-09-17 12:07:47 -0700

    contact page

---
+++ wp-content/themes/html5blank-stable3-child/template-contact.php
@@ -0,0 +1,24 @@
+<?php /* Template Name: Contact Template */ get_header(); ?>
+
+  <main role="main">
+    <!-- section -->
+    <section>
+
+			<div class="container" id="startchange">
+				<div class="row">
+					<div class="col-md-12">
+					 	<h1 class="contactKatie">
+						 	<span class="scriptedTitle">Contact Katie Williamson</span>
+						</h1>
+
+					
+				    <div class="contact">
+					
+              <?php echo do_shortcode('[contact-form-7 id="31" title="Contact form 1"]');?>
+				    </div>
+          </div>
+			   </div>
+        </div>		
+      </section>
+
+<?php get_footer(); ?>
