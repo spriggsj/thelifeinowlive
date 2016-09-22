d9cce289f3b497a431d7c610b095e2b5c4c85cf5
jason spriggs <jasonspriggs73@gmail.com>
2016-09-21 21:09:27 -0700

    analytics

--- wp-content/themes/html5blank-stable3-child/header.php
+++ wp-content/themes/html5blank-stable3-child/header.php
@@ -23,7 +23,18 @@
     	<?php wp_head(); ?>
 
 
+		<meta name="google-site-verification" content="kgQiUkQVJ6es6NlZfJrGMOfqJFYPJqOgQnhiE_0TE1I" />
+    
+      <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-84597161-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 
     </head>
 
